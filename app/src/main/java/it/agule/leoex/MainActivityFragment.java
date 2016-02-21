e215f8fa37eb71e33f405df0d6be936d1af35921
AGule <Andriy.Gule@gmail.com>
2016-02-21 12:13:53 +0100

    ListView adapter setup, yet with fake data

--- app/src/main/java/it/agule/leoex/MainActivityFragment.java
+++ app/src/main/java/it/agule/leoex/MainActivityFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mTimetableAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_timetable);
+
+        String[] timetableArray = { // some fake list items...
+                "Hello World!", "item2", "item3", "item4", "item5", "item6", "item7"
+        };
+        List<String> strListData = new ArrayList<String>(Arrays.asList(timetableArray));
+        mTimetableAdapter = new ArrayAdapter<String>(getActivity(),
+                R.layout.timetable_item, R.id.list_item_timetable, strListData);
+        listView.setAdapter(mTimetableAdapter);
+
+        return rootView;
     }
 }
