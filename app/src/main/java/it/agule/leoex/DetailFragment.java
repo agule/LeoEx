18d72faccd2f5b48bf40ee3e1ef7e775c68696f8
AGule <Andriy.Gule@gmail.com>
2016-02-28 22:03:57 +0100

    Added Detail Activity - passing current string to display

--- app/src/main/java/it/agule/leoex/DetailFragment.java
+++ app/src/main/java/it/agule/leoex/DetailFragment.java
@@ -1,10 +1,12 @@
 package it.agule.leoex;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,14 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        // The detail Activity called via intent.  Inspect the intent for text data.
+        Intent intent = getActivity().getIntent();
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.text)).setText(forecastStr);
+        }
+
+        return rootView;
     }
 }
